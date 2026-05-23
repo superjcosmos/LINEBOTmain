97fda1d9cb15d16f1297ca152af57bcf4893d6eb
superjcosmos <superjcosmos@gmail.com>
2026-05-23 18:52:41 +0800

    Delete pages directory

--- pages/audience.js
+++
@@ -1,7 +0,0 @@
-// audience.js
-async function loadAudience() {
-  setContent(
-    '<h2 class="page-title">受眾管理</h2>' +
-    '<div class="card"><div class="loading">開發中...</div></div>'
-  );
-}
--- pages/richmenu.js
+++
@@ -1,7 +0,0 @@
-// richmenu.js
-async function loadRichMenu() {
-  setContent(
-    '<h2 class="page-title">圖文選單</h2>' +
-    '<div class="card"><div class="loading">開發中...</div></div>'
-  );
-}
