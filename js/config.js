b4534dc45548266d1f5ed14b3a9c6f216abc0609
superjcosmos <superjcosmos@gmail.com>
2026-05-24 11:26:27 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 var CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxwt06KTNwblLF0JnM-tZ2kjjGP6SC6N4ZEX6YUbm7e7IKP23c9IH0B1gskNykZ8XiR/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbzMfbfL8b3iabnywmXoncowavW3uK-wLZE53cZTB3C5yKuq4_4xC3VplOgaqSLOEIij9w/exec"
 };
