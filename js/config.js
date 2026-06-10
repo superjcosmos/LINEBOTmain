5e60959e6337525c49105ca803084f6be0911354
superjcosmos <superjcosmos@gmail.com>
2026-06-10 09:21:30 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 
 var CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzMfbfL8b3iabnywmXoncowavW3uK-wLZE53cZTB3C5yKuq4_4xC3VplOgaqSLOEIij9w/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbzMfbfL8b3iabnywmXoncowavW3uK-wLZE53cZTB3C5yKuq4_4xC3VplOgaqSLOEIij9w/exec",
   VERSION: 'v0.9.0' 
 };
